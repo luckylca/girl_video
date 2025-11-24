89dd1ceda30ba61284a6d5479357a5301baf23bd
lucky <101040016+luckylca@users.noreply.github.com>
2025-11-25 01:42:59 +0800

    完成视频封面的设置

--- store/videoList.js
+++ store/videoList.js
@@ -2,6 +2,7 @@ import { defineStore } from "pinia"
 import { ref, watch } from 'vue'
 export const videoListStore = defineStore("videoList",()=>{
 	const videoList = ref([])
+	const coverList = ref([])
 	const drawContent = ref([
 		{"value": 0,"text": "小姐姐1","videoList":[],"coverList":[]},
 		{"value": 1,"text": "小姐姐2","coverList":[],"videoList":[]}
@@ -9,6 +10,9 @@ export const videoListStore = defineStore("videoList",()=>{
 	
 	const addVideoItem = (videoItem)=>{
 		videoList.value.push(videoItem)
+	}
+	const addCoverItem = (coverItem)=>{
+		coverList.value.push(coverItem)
 	}
 	const addDrawContent = (drawItem) => {
 		drawContent.value.push(drawItem)
@@ -27,5 +31,12 @@ export const videoListStore = defineStore("videoList",()=>{
 	  },
 	  { deep: true }
 	)
-	return {videoList,addVideoItem,drawContent,addDrawContent}
+	watch(
+	  () => coverList.value,
+	  (newVal) => {
+	    uni.setStorageSync('coverList', JSON.stringify(newVal))
+	  },
+	  { deep: true }
+	)
+	return {videoList,addVideoItem,drawContent,addDrawContent,coverList,addCoverItem}
 })
